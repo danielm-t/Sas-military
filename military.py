f255750ced95a99bac1e23684b4ad7f74c6cfb22
Daniel <dntudose@gmail.com>
2023-02-19 11:12:56 +0000

    Update military.py

--- military.py
+++ military.py
@@ -1,17 +1,28 @@
-from datetime import datetime
 from num2words import num2words
-string= input("")
-time= datetime.strptime(string, "%I:%M%p")
-time=time.strftime("%H:%M")
-hours,minutes=[int(x)for x in time.split(":")]
+time_str= input("")
+tz=time_str[-2:]
+hours, minutes = map(int, time_str[:-2].split(':'))
 output=""
-if(hours<10):
-    output="zero "+num2words(hours)
-if (minutes==0):
-    output+=" hundred hours"
-elif (minutes<10):
-    output+=" zero "+num2words(minutes)
-print(output)
+if(tz=="PM"):
+    hours=hours+12
+    output+=num2words(hours)+" "
+else:
+    if(hours==12):
+        hours=0
+        output+=num2words(hours)+" "
+    else:
+        if(hours<10):
+            output+=num2words(0)+" "+ num2words(hours)+ " "
+        else:
+            output+=num2words(hours)+" "
+    
 
+if minutes == 0:
+    output+="hundred hours"
+elif minutes < 10:
+    output+="zero "+num2words(minutes)
+else:
+    output+=num2words(minutes)
+print(output.replace("-"," "))
 
 
